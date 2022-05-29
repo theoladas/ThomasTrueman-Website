158d0ca2de5c4514a171bfa67e31a8bf5bc2ed69
Theo <theodorosladasmail@gmail.com>
2022-05-29 22:37:38 +0100

    Added - on scroll animation scene

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function animateSlides() {
   // Select elements
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
+
   // Loop over each slide
   sliders.forEach((slide) => {
     // Select some other elements
@@ -19,11 +20,29 @@ function animateSlides() {
     const slideTimeline1 = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
     });
+
     // Create Animations
     slideTimeline1.fromTo(revealImage, { x: "0%" }, { x: "100%" });
     slideTimeline1.fromTo(image, { scale: 1.5 }, { scale: 1 }, "-=1");
     slideTimeline1.fromTo(revealtext, { x: "0%" }, { x: "100%" }, "-=0.75");
     slideTimeline1.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.8");
+
+    // Create Scene so we can animate when we scroll
+    slideScene = new ScrollMagic.Scene({
+      // we want to start on every slide
+      triggerElement: slide,
+      triggerHook: 0.35,
+      // to be visible always the slide when we trigger the scroll
+      reverse: false,
+    })
+      // to copy the previous timeline animation we use .setTween()
+      .setTween(slideTimeline1)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
   });
 }
 // Call the function
